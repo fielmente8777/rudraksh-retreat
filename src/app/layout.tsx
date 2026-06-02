4fff3bcd195380e54080a8db5b21f1a9cd833f38
Personal <sk7627577@gmail.com>
2026-06-02 12:55:06 +0530

    feat: Add Facebook Pixel tracking code and corresponding noscript image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Cormorant_Garamond, Inter } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
 import "./style.scss";
+import Image from "next/image";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -15,7 +16,6 @@ const cormorant = Cormorant_Garamond({
   subsets: ["latin"],
 });
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -24,6 +24,30 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* <!-- Meta Pixel Code --> */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+        {`!function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1690218598834649');
+        fbq('track', 'PageView');
+        `}
+        </Script>
+        <noscript>
+          <Image
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            alt="Facebook Pixel"
+            src="https://www.facebook.com/tr?id=1690218598834649&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* <!-- End Meta Pixel Code --> */}
         {/* <!-- Google Tag Manager --> */}
         <Script
           strategy="afterInteractive"
